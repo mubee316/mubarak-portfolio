4ee31fc8b3da2d3c3dba38b4498058adc6262c79
mubarak olalekan <mubarakolalekan316@gmail.com>
2024-09-24 12:20:09 +0100

    myportfolio

---
+++ app/Header/page.tsx
@@ -0,0 +1,66 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+
+const Header = () => {
+  return (
+    <>
+      <div className='flex flex-row items-center justify-between border bg-blus h-20 fixed top-0 w-full z-10 bg-blus/90 backdrop-blur-md px-4 md:px-10'>
+      
+        <div className='text-white font-bold'>
+          <h1 className='text-lg md:text-2xl'>OLALEKAN MUBARAK</h1>
+          <p className='font-light font-mono text-xs md:text-sm'>
+            Software Engineer (Front-End)
+          </p>
+        </div>
+
+        <div className='flex space-x-3 md:space-x-5 items-center'>
+          <AlertDialog>
+  <AlertDialogTrigger>          <Button className='bg-transparent text-white text-xs md:text-sm'>
+  About Me
+  </Button>
+  </AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle className='font-bold text-1xl text-center text-blus'>Building the Future, One Line of Code at a Time</AlertDialogTitle>
+      <AlertDialogDescription>
+      Hello, I'm Mubarak Olalekan, a passionate and detail-oriented frontend developer with a strong focus on creating engaging, user-friendly web applications. With expertise in technologies such as Next.js, React, TypeScript, and Tailwind CSS, I thrive on transforming ideas into responsive, interactive digital experiences. My work is driven by a love for clean code, efficient design, and intuitive user interfaces.
+
+I am committed to continuous learning and growth in the tech field, constantly expanding my knowledge and keeping up with the latest industry trends. Whether I'm building a URL shortening app with analytics, integrating third-party APIs, or working on personal projects, I aim to deliver functional, elegant solutions that meet both user and business needs.
+
+In addition to development, I have an entrepreneurial spirit, managing a small business where I combine creativity with technical skills to offer unique digital solutions. My goal is to contribute to innovative projects, collaborate with like-minded professionals, and continue pushing the boundaries of what modern web technologies can achieve.
+<br /><br />
+Let’s connect and create something exceptional!s.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel className='bg-blus text-white hover:border border-blus '>Cancel</AlertDialogCancel>
+      {/* <AlertDialogAction>Continue</AlertDialogAction> */}
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
+          <Button className='bg-transparent text-white text-xs md:text-sm'>
+            Contact
+          </Button>
+          <Button className='bg-white text-black text-xs md:text-sm'>
+            Hire Me
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,176 @@
+"use client";
 import Image from "next/image";
+import Header from "./Header/page";
+import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
 
-export default function Home() {
+const Page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="overflow-hidden">
+      <Header />
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-center ">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            delay: 0.2,
+            x: { type: "spring", stiffness: 60 },
+            opacity: { duration: 0.6 },
+            ease: "easeIn",
+            duration: 1,
+          }}
+          className="text-lg  mt-20 md:mt-52 md:ml-10 md: text-center"
+        >
+          <div>
+            <h1 className="text-2xl md:text-4xl font-bold">
+              <span className="text-blus">Optimizing</span> the <br />
+              web <span className="text-blus">experience</span>
+              <br /> one line of{" "}
+              <span className="underline text-blus">
+                code <br />
+                at a time
+              </span>
+            </h1>
+          </div>
+          <div className="text-sm md:text-[20px] mt-4 font-mono text-gray-500">
+            Hi there! I'm Mubarak, a <br />
+            frontend developer focused on creating responsive,
+            <br /> intuitive, and efficient web interfaces{" "}
+          </div>
+          <div className="flex justify-center mt-4 space-x-5">
+            <a href="https://github.com/mubee316">
+              <Button className="bg-blus flex gap-2 font-mono text-[15px] hover:bg-white hover:text-blus hover: border border-blus active:bg-blus active:text-white">
+                <FaGithub />
+                Github
+              </Button>
+            </a>
+            <a
+              href="www.linkedin.com/in/
+mubarak-olalekan-106a20304"
+            >
+              <Button className="bg-white text-blus border border-blus flex gap-2 font-mono text-[15px] hover:text-white hover:bg-blus active:bg-white active:text-blus">
+                <FaLinkedin />
+                Linkedin
+              </Button>
+            </a>
+          </div>
+        </motion.div>
+
+        <motion.img
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          src="/manu.png"
+          alt="Portrait of Mubarak Olalekan"
+          width={350}
+          height={500}
+          className="mt-10 md:mt-36"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mt-32 md:mt-56 text-center">
+        <h1 className="text-xl md:text-3xl font-bold">
+          Crafting Modern and Intuitive Digital Experiences for the Web:
+        </h1>
+        <p className="mt-2">Discover My Work: A Tour of My Projects</p>
       </div>
-    </main>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 md:mt-20 px-4 md:px-10">
+        <motion.div 
+           initial={{ x: -100, opacity: 0 }}
+           whileInView={{ x: 0, opacity: 1 }}
+           transition={{
+             delay: 0.2,
+             x: { type: "spring", stiffness: 60 },
+             opacity: { duration: 0.6 },
+             ease: "easeIn",
+             duration: 1,
+           }}><ProjectCard  /></motion.div>
+        <motion.div
+           initial={{ y: 100, opacity: 0 }}
+           whileInView={{ y: 0, opacity: 1 }}
+           transition={{
+             delay: 0.2,
+             y: { type: "spring", stiffness: 60 },
+             opacity: { duration: 0.6 },
+             ease: "easeIn",
+             duration: 0.2,
+           }}><ProjectCard2 /></motion.div>
+        <motion.div 
+           initial={{ x: 100, opacity: 0 }}
+           whileInView={{ x: 0, opacity: 1 }}
+           transition={{
+             delay: 0.2,
+             x: { type: "spring", stiffness: 60 },
+             opacity: { duration: 0.6 },
+             ease: "easeIn",
+             duration:3 ,
+           }}><ProjectCard /></motion.div>
+      </div>
+      <div>dfgfgjhjkl;l;
+        
+      </div>
+    </div>
   );
-}
+};
+
+const ProjectCard = () => (
+  <div className="border border-blus rounded-xl w-full h-auto flex flex-col text-center p-4 ">
+    <p className="animate-bounce mt-2 text-xl font-bold">DIGITAL CLOCK</p>
+    <Image
+      src="/nnnn.png"
+      width={350}
+      height={250}
+      alt="Digital clock project"
+      className="mx-auto hover:transition duration-300 ease-in-out transform hover:scale-110 bg-blus"
+    />
+    <p className="p-4">
+      I created a digital clock using HTML, CSS, and JavaScript. The clock
+      dynamically displays the current time by using JavaScript functions to
+      retrieve the time and update it at regular intervals. This provides a
+      real-time clock that users can view directly in their browser. The project
+      focuses on integrating basic web technologies to create a functional and
+      visually appealing digital clock interface.
+    </p>
+
+    <div className="flex justify-between px-8 mt-4">
+      <Button className="bg-blus">VIEW LIVE</Button>
+      <Button className="bg-white text-blus border border-blus hover:text-white">
+        GITHUB REPO
+      </Button>
+    </div>
+  </div>
+);
+const ProjectCard2 = () => (
+  <div className="border border-blus rounded-xl w-full h-auto flex flex-col text-center p-4 bg-blus text-white">
+    <p className="animate-bounce mt-2 text-xl font-bold">FOOD RECIPE FINDER</p>
+    <Image
+      src="/recipe.png"
+      width={350}
+      height={250}
+      alt="food recipe project"
+      className="mx-auto hover:transition duration-300 ease-in-out transform hover:scale-110 bg-blus"
+    />
+    <p className="p-4">
+      The Food Recipe Finder is a web application I built to help users discover
+      new and exciting recipes. Using a recipe API from RapidAPI, the
+      application allows users to search for recipes based on ingredients,
+      cuisine types, or dish names. The aim of the project was to create an
+      intuitive, easy-to-use platform that gives quick access to a wide variety
+      of meal ideas and cooking inspiration.
+    </p>
+
+    <div className="flex justify-between px-8 mt-4">
+      <Button className="bg-blus border border-orange-600 text-orange-600 hover:bg-white">VIEW LIVE</Button>
+      <Button className="bg-orange-600 text-white border border-blus hover:text-orange hover:border border-orange-600 hover:bg-blus hover:text-orange-600">
+        GITHUB REPO
+      </Button>
+    </div>
+  </div>
+);
+
+export default Page;
